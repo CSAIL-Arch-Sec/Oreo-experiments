9e3caf78f3bd906453088b014c1e92b86df49beb
Joseph Zhang <jzha@mit.edu>
2023-07-13 15:41:04 -0400

    clean up save a bit

--- gem5-configs/x86-save.py
+++ gem5-configs/x86-save.py
@@ -11,22 +11,81 @@ from gem5.components.processors.simple_processor import (
 )
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.isas import ISA
-from gem5.resources.resource import (
-    Resource,
-    CustomDiskImageResource,
-)
+from gem5.resources.resource import *
 from gem5.simulate.simulator import Simulator
 from gem5.simulate.exit_event import ExitEvent
 
+from common import ObjectList
+
 parser = argparse.ArgumentParser(
-    description="config script for checkpoint saver"
+    description = "configuration script for checkpoint generation"
 )
+
+cpu_choices = list(CPUTypes.__members__.keys())
+
+# parser.add_argument(
+#     "--list-cpu-types",
+#     action = "callback",
+#     callback = [print(choice) for choice in cpu_choices],
+#     help = "list cpu types"
+# )
+
+parser.add_argument(
+    "--cpu-type",
+    type = lambda name: CPUTypes.__members__.get(name),
+    default = CPUTypes.KVM,
+    help = "cpu type for run",
+    choices = list(CPUTypes.__members__.values()),
+)
+
+# cpu cores
+
+# def positive_int_arg_filter(value):
+#     int_value = int(value)
+#     if int_value <= 0:
+#         raise argparse.ArgumentTypeError(f"{int_value} not positive int")
+#     return int_value
+
+parser.add_argument(
+    "--cpu-cores",
+    # type = positive_int_arg_filter,
+    type = int,
+    default = 1,
+    help = "number of cpu cores for run"
+)
+
+# kernel and disk image
+
+parser.add_argument(
+    "--kernel",
+    type = lambda path: CustomResource.__init__(str(path)),
+    default = Resource("x86-linux-kernel-4.19.83"),
+    help = "kernel to use for run"
+)
+
+parser.add_argument(
+    "--disk-image-path",
+    type = str,
+    default = "/root/experiments/disk-image/syscall/syscall-image/syscall",
+    help = "disk image path to use for run"
+)
+
+parser.add_argument(
+    "--disk-root-partition",
+    type = str,
+    default = "1",
+    help = "root partiton of disk image"
+)
+
+# parse args blah
+
+args = parser.parse_args()
 
 # We check for the required gem5 build.
 
 requires(
-    isa_required=ISA.X86,
-    kvm_required=True,
+    isa_required = ISA.X86,
+    kvm_required = ObjectList.is_kvm_cpu(args.cpu_type),
 )
 
 from gem5.components.cachehierarchies.classic.no_cache import NoCache
@@ -37,18 +96,18 @@ from gem5.components.cachehierarchies.classic.no_cache import NoCache
 memory = DualChannelDDR4_2400(size="3GB")
 
 processor = SimpleProcessor(
-    cpu_type=CPUTypes.KVM,
-    isa=ISA.X86,
-    num_cores=2,
+    cpu_type = args.cpu_type,
+    isa = ISA.X86,
+    num_cores = args.cpu_cores,
 )
 
 # Here we setup the board. The X86Board allows for Full-System X86 simulations
 
 board = X86Board(
-    clk_freq="3GHz",
-    processor=processor,
-    memory=memory,
-    cache_hierarchy=NoCache(),
+    clk_freq = "3GHz",
+    processor = processor,
+    memory = memory,
+    cache_hierarchy = NoCache(),
 )
 
 board.set_kernel_disk_workload(
@@ -56,8 +115,8 @@ board.set_kernel_disk_workload(
     # `~/.cache/gem5` directory if not already present.
     kernel=Resource("x86-linux-kernel-4.19.83"),
     disk_image=CustomDiskImageResource(
-        "/root/experiments/disk-image/syscall/syscall-image/syscall",
-        disk_root_partition="1"
+        local_path = args.disk_image_path,
+        disk_root_partition = args.disk_root_partition
     ),
 )
 
@@ -66,14 +125,13 @@ def handle_checkpoint():
     yield True
 
 simulator = Simulator(
-    board=board,
-    on_exit_event={
+    board = board,
+    on_exit_event = {
         ExitEvent.CHECKPOINT: handle_checkpoint(),
     },
 )
 
-print("Running the simulation")
-print("Using KVM cpu")
+print("Starting simulation")
 
 simulator.run()
 
