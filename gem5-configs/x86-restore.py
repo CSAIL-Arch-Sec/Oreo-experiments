de3e6fadb31e19a707b4102d2f13cd7fba727db9
Joseph Zhang <jzha@mit.edu>
2023-07-13 20:44:53 -0400

    some reading json shenanigans is nonfunctional rn for lazy testing

--- gem5-configs/x86-restore.py
+++ gem5-configs/x86-restore.py
@@ -1,35 +1,36 @@
 import argparse
+import json
 import os
 
-import m5
+# import m5
 
-from gem5.utils.requires import requires
-from gem5.components.boards.x86_board import X86Board
-from gem5.components.memory import DualChannelDDR4_2400
-from gem5.components.processors.simple_processor import (
-    SimpleProcessor,
-)
-from gem5.components.processors.cpu_types import CPUTypes
-from gem5.isas import ISA
-from gem5.coherence_protocol import CoherenceProtocol
-from gem5.resources.resource import (
-    Resource,
-    CustomDiskImageResource,
-)
-from gem5.simulate.simulator import Simulator
-from gem5.simulate.exit_event import ExitEvent
+# from gem5.utils.requires import requires
+# from gem5.components.boards.x86_board import X86Board
+# from gem5.components.memory import DualChannelDDR4_2400
+# from gem5.components.processors.simple_processor import (
+#     SimpleProcessor,
+# )
+# from gem5.components.processors.cpu_types import CPUTypes
+# from gem5.isas import ISA
+# from gem5.coherence_protocol import CoherenceProtocol
+# from gem5.resources.resource import (
+#     Resource,
+#     CustomDiskImageResource,
+# )
+# from gem5.simulate.simulator import Simulator
+# from gem5.simulate.exit_event import ExitEvent
 
 parser = argparse.ArgumentParser(
     description = "configuration script for checkpoint restore"
 )
 
-parser.add_argument(
-    "--cpu-type",
-    type = lambda name: CPUTypes.__members__.get(name),
-    default = CPUTypes.KVM,
-    help = "cpu type for checkpoint generation",
-    choices = list(CPUTypes.__members__.values()),
-)
+# parser.add_argument(
+#     "--cpu-type",
+#     type = lambda name: CPUTypes.__members__.get(name),
+#     default = CPUTypes.KVM,
+#     help = "cpu type for checkpoint generation",
+#     choices = list(CPUTypes.__members__.values()),
+# )
 
 parser.add_argument(
     "--script",
@@ -38,82 +39,123 @@ parser.add_argument(
     help = "path to script to run ??"
 )
 
+# stuff to help us restore
+
+parser.add_argument(
+    "--checkpoint-dir",
+    type = str,
+    default = None,
+    help = "directory of checkpoint to restore from"
+)
+
+parser.add_argument(
+    "--checkpoint-id",
+    type = str,
+    default = None,
+    help = "id of checkpoint to restore from"
+)
+
 args = parser.parse_args()
 
-# We check for the required gem5 build.
+# things for reading generated checkpoint config
 
-requires(
-    isa_required = ISA.X86,
-    coherence_protocol_required = CoherenceProtocol.MESI_TWO_LEVEL,
-)
+if not (args.checkpoint_dir or args.checkpoint_id):
+    parser.error('No checkpoint specified, please provide one of --checkpoint-dir or --checkpoint-id, thanks :D')
+elif args.checkpoint_dir and args.checkpoint_id:
+    parser.error('Please specify only one of --checkpoint-dir or --checkpoint-id, thanks :D')
 
-# Setting up all the fixed system parameters here
-# Caches: MESI Two Level Cache Hierarchy
+checkpoint_dir = args.checkpoint_dir or \
+    f"/Users/jzha/protect-kaslr/experiments/m5outs/{args.checkpoint_id}/m5out-gen-cpt"
+    # f"/root/protect-kaslr/experiments/m5outs/{args.checkpoint_id}/m5out-gen-cpt"
 
-from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
-    MESITwoLevelCacheHierarchy,
-)
+with open(f"{checkpoint_dir}/config.json") as f:
+    config = json.load(f)
 
-cache_hierarchy = MESITwoLevelCacheHierarchy(
-    l1d_size="32kB",
-    l1d_assoc=8,
-    l1i_size="32kB",
-    l1i_assoc=8,
-    l2_size="256kB",
-    l2_assoc=16,
-    num_l2_banks=2,
-)
+cpu_cores = len(config.get("board").get("processor").get("cores"))
+print(f'num_cores: {cpu_cores}')
 
-# Memory: Dual Channel DDR4 2400 DRAM device.
-# The X86 board only supports 3 GB of main memory.
+kernel_path = config.get("board").get("workload").get("object_file")
+print(f'kernel path: {kernel_path}')
 
-memory = DualChannelDDR4_2400(size="3GB")
+disk_image_paths = [disk.get("image").get("child").get("image_file") for disk in \
+    config.get("board").get("pc").get("south_bridge").get("ide").get("disks")]
+disk_image_path = disk_image_paths[0]
+print(f'disk path: {disk_image_paths}')
 
-processor = SimpleProcessor(
-    cpu_type = args.cpu_type,
-    isa = ISA.X86,
-    num_cores = 2,
-)
+# # We check for the required gem5 build.
 
-# Here we setup the board. The X86Board allows for Full-System X86 simulations
+# requires(
+#     isa_required = ISA.X86,
+#     coherence_protocol_required = CoherenceProtocol.MESI_TWO_LEVEL,
+# )
 
-board = X86Board(
-    clk_freq = "3GHz",
-    processor = processor,
-    memory = memory,
-    cache_hierarchy = cache_hierarchy,
-)
+# # Setting up all the fixed system parameters here
+# # Caches: MESI Two Level Cache Hierarchy
 
-board.set_kernel_disk_workload(
-    # The x86 linux kernel will be automatically downloaded to the
-    # `~/.cache/gem5` directory if not already present.
-    kernel=Resource("x86-linux-kernel-4.19.83"),
-    disk_image=CustomDiskImageResource(
-        "/root/experiments/disk-image/syscall/syscall-image/syscall",
-        disk_root_partition="1"
-    ),
-    readfile = args.script,
-)
+# from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
+#     MESITwoLevelCacheHierarchy,
+# )
 
-def handle_checkpoint():
-    m5.checkpoint(m5.options.outdir)
-    yield True
+# cache_hierarchy = MESITwoLevelCacheHierarchy(
+#     l1d_size="32kB",
+#     l1d_assoc=8,
+#     l1i_size="32kB",
+#     l1i_assoc=8,
+#     l2_size="256kB",
+#     l2_assoc=16,
+#     num_l2_banks=2,
+# )
 
-simulator = Simulator(
-    board=board,
-    on_exit_event={
-        ExitEvent.CHECKPOINT: handle_checkpoint(),
-    },
-    checkpoint_path=m5.options.outdir,
-)
+# # Memory: Dual Channel DDR4 2400 DRAM device.
+# # The X86 board only supports 3 GB of main memory.
 
-# We maintain the wall clock time.
+# memory = DualChannelDDR4_2400(size="3GB")
 
-print("Running the simulation")
-print("Using KVM cpu")
+# processor = SimpleProcessor(
+#     cpu_type = args.cpu_type,
+#     isa = ISA.X86,
+#     num_cores = 2,
+# )
 
-# We start the simulation
-simulator.run()
+# # Here we setup the board. The X86Board allows for Full-System X86 simulations
 
-print("All simulation events were successful.")
-print("Done with the simulation")
+# board = X86Board(
+#     clk_freq = "3GHz",
+#     processor = processor,
+#     memory = memory,
+#     cache_hierarchy = cache_hierarchy,
+# )
+
+# board.set_kernel_disk_workload(
+#     # The x86 linux kernel will be automatically downloaded to the
+#     # `~/.cache/gem5` directory if not already present.
+#     kernel=Resource("x86-linux-kernel-4.19.83"),
+#     disk_image=CustomDiskImageResource(
+#         "/root/experiments/disk-image/syscall/syscall-image/syscall",
+#         disk_root_partition="1"
+#     ),
+#     readfile = args.script,
+# )
+
+# def handle_checkpoint():
+#     m5.checkpoint(m5.options.outdir)
+#     yield True
+
+# simulator = Simulator(
+#     board=board,
+#     on_exit_event={
+#         ExitEvent.CHECKPOINT: handle_checkpoint(),
+#     },
+#     checkpoint_path=m5.options.outdir,
+# )
+
+# # We maintain the wall clock time.
+
+# print("Running the simulation")
+# print("Using KVM cpu")
+
+# # We start the simulation
+# simulator.run()
+
+# print("All simulation events were successful.")
+# print("Done with the simulation")
