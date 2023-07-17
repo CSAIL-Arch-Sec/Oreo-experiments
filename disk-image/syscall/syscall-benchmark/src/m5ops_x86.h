4f3d7bdaadc92cce84ea7a5f826e42e0f7fd6339
Joseph Zhang <josephzhang268@gmail.com>
2023-07-17 12:12:38 -0400

    .

--- disk-image/syscall/syscall-benchmark/src/m5ops_x86.h
+++ disk-image/syscall/syscall-benchmark/src/m5ops_x86.h
@@ -1,11 +1,3 @@
-#define M5_RESET_STATS \
-    mov $0, %rdi \
-    mov $0, %rsi \
-    .byte 0x0F, 0x04 \
-    .word 0x40
+#define M5_RESET_STATS mov $0, %rdi; mov $0, %rsi; .byte 0x0F, 0x04; .word 0x40
 
-#define M5_DUMP_STATS \
-    mov $0, %rdi \
-    mov $0, %rsi \
-    .byte 0x0F, 0x04 \
-    .word 0x41
+#define M5_DUMP_STATS mov $0, %rdi; mov $0, %rsi; .byte 0x0F, 0x04; .word 0x41
