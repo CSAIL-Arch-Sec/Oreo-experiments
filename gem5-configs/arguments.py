4b6030986b13f71c0e7a393cc6801873220c216a
Joseph Zhang <josephzhang268@gmail.com>
2023-08-08 06:45:39 -0400

    arguments documentation

--- gem5-configs/arguments.py
+++ gem5-configs/arguments.py
@@ -3,89 +3,128 @@ from gem5.resources.resource import *
 
 m5outs_default_dir = "/root/experiments/m5outs"
 
-def add_std_redirect_arguments(parser):
 
+def add_std_redirect_arguments(parser):
+    '''
+    add arguments for redirecting stderr/stdout to file
+    '''
     parser.add_argument(
         "--redirect-stderr",
         action = "store_true",
-        default = True
+        default = True,
+        help = "enable redirecting stderr to file"
     )
     parser.add_argument(
         "--no-redirect-stderr",
         dest = "redirect-stderr",
-        action = "store_false"
+        action = "store_false",
+        help = "disable redirecting stderr to file"
     )
     parser.add_argument(
         "--stderr-file",
         type = str,
-        default = None
+        default = None,
+        help = "specify file to redirect stderr to"
     )
     parser.add_argument(
         "--redirect-stdout",
         action = "store_true",
-        default = False
+        default = False,
+        help = "enable redirecting stdout to file"
     )
     parser.add_argument(
         "--no-redirect-stdout",
         dest = "redirect-stdout",
-        action = "store_false"
+        action = "store_false",
+        help = "disable redirecting stdout to file"
     )
     parser.add_argument(
         "--stdout-file",
         type = str,
-        default = None
+        default = None,
+        help = "specify file to redirect stdout to"
     )
 
+
 def add_debug_arguments(parser):
+    '''
+    add arguments for saving debug trace to file
+    '''
     parser.add_argument(
         '--debug-file',
         type = str,
-        default = None
+        default = None,
+        help = "specify file to save debug trace to"
     )
 
 
 def add_checkpoint_restore_arguments(parser):
+    '''
+    add arguments for configuring restoring from checkpoint
+    '''
     parser.add_argument(
         "--checkpoint-dir",
         type = str,
         default = None,
-        help = "directory of checkpoint to restore from"
+        help = "specify directory of checkpoint to restore from"
     )
     parser.add_argument(
         "--checkpoint-id",
         type = str,
         default = None,
-        help = "id of checkpoint to restore from"
+        help = "specify id of checkpoint to restore from"
     )
     parser.add_argument(
         "--checkpoint-tick",
         type = int,
         default = None,
-        help = "for restoring tick checkpoint"
+        help = "specify tick of checkpoint to restore from"
     )
     parser.add_argument(
         "--checkpoint-latest",
         action = "store_true",
         default = False,
-        help = "restore from most recent checkpoint"
+        help = "enable automatic restoring from most recent checkpoint"
     )
     add_uuid_dir_argument(parser)
 
+
 def add_checkpoint_save_arguments(parser):
+    '''
+    add arguments for configuring saving a checkpoint
+    '''
     parser.add_argument(
         "--outputs-dir",
         type = str,
-        default = m5outs_default_dir
+        default = m5outs_default_dir,
+        help = "specify output directory"
     )
     parser.add_argument(
         '--checkpoint',
         type = str,
         nargs = '+',
-        default = ""
+        default = "",
+        help = "specify ticks to schedule checkpoints at"
     )
     add_uuid_dir_argument(parser)
 
+
+def add_uuid_dir_argument(parser):
+    '''
+    add arguments for enabling uuid saves (this is kinda clunky though ;-;)
+    '''
+    parser.add_argument(
+        "--uuid-dir",
+        action = "store_true",
+        default = False,
+        help = "enable uuid based output naming"
+    )
+
+
 def add_kernel_disk_arguments(parser):
+    '''
+    add arguments for kernel disk workload parameters
+    '''
     parser.add_argument(
         "--kernel",
         type = lambda path: CustomResource(str(path)),
@@ -105,15 +144,23 @@ def add_kernel_disk_arguments(parser):
         help = "root partiton of disk image"
     )
 
+
 def add_run_script_arguments(parser):
+    '''
+    add arguments for command script configuration
+    '''
     parser.add_argument(
         "--script",
         type = str,
         default = "/root/experiments/command-scripts/exit_immediate.rcS",
-        help = "path to script to run ??"
+        help = "path to script to run"
     )
 
+
 def add_cpu_arguments(parser, default_type: CPUTypes):
+    '''
+    add arguments for cpu configuration
+    '''
     parser.add_argument(
         "--cpu-type",
         type = lambda name: CPUTypes.__members__.get(str(name)),
@@ -128,43 +175,47 @@ def add_cpu_arguments(parser, default_type: CPUTypes):
         help = "number of cpu cores for run"
     )
 
-def add_uuid_dir_argument(parser):
-    parser.add_argument(
-        "--uuid-dir",
-        action = "store_true",
-        default = False
-    )
 
 def add_kernel_phy_load_arguments(parser):
+    '''
+    add arguments for configuring kernel physical load address
+    '''
     val_to_int = lambda val: int(val, 0)
-
     parser.add_argument(
         "--load-addr-mask",
         type = val_to_int,
-        default = 0xFFFFFFFFFFFFFFFF
+        default = 0xFFFFFFFFFFFFFFFF,
+        help = "specify kernel physical load address mask"
     )
-
     parser.add_argument(
         "--load-addr-offset",
         type = val_to_int,
-        default = 0
+        default = 0,
+        help = "specify kernel physical load address offset"
     )
-
     parser.add_argument(
         '--addr-check', 
         action = 'store_true',
-        default = True
+        default = True,
+        help = "enable kernel load address check"
     )
-
     parser.add_argument(
         '--no-addr-check', 
         dest = 'addr_check', 
-        action = 'store_false'
+        action = 'store_false',
+        help = "disable kernel load address check"
     )
 
+
 def add_cache_arguments(parser):
+    '''
+    add arguments for configuring cache
+    '''
     parser.add_argument(
         '--ruby-cache', 
         action = 'store_true',
-        default = False
+        default = False,
+        help = "enable ruby cache"
     )
+    # TODO: unsure if the MOESI_hammer actually worked or not
+    # or actually is checkpointing with cache even needed??
