68389ee4dbe56fcd1d6d7b6f531d6ccaf9c5318b
Joseph Zhang <josephzhang268@gmail.com>
2023-08-20 11:21:39 -0400

    split post install script into two parts

---
+++ disk-image/experiments/experiments-post-installation.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2020 The Regents of the University of California.
+# SPDX-License-Identifier: BSD 3-Clause
+
+#!/bin/bash
+echo 'Post Installation Started'
+
+sudo chown -R gem5:gem5 /home/gem5/experiments/
+
+echo 'Post Installation Done'
