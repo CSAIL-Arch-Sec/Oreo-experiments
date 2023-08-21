14ec0791ff52c9c09c1fe2573130d6347d9eb2f0
Joseph Zhang <josephzhang268@gmail.com>
2023-08-21 06:01:59 -0400

    make return value work?

--- disk-image/experiments/experiments/include/m5ops.h
+++ disk-image/experiments/experiments/include/m5ops.h
@@ -87,29 +87,30 @@
 
 
 // hardcode magic instructions 
-#define __M5_OP_INST(num, ...) __asm__ __volatile__ (__M5_OP_INST_ASM_STR(num) : : __VA_ARGS__ :)
+#define __M5_OP_INST(num, out, ...) __asm__ __volatile__ (__M5_OP_INST_ASM_STR(num) : out : __VA_ARGS__ :)
 #define __M5_OP_INST_ASM(num) .word 0x040F; .word num;
 #define __M5_OP_INST_ASM_STR(num) STRINGIFY(__M5_OP_INST_ASM(num))
-#define M5_OP_INST(num, ...) __M5_OP_INST(num, __VA_ARGS__)
+#define M5_OP_INST(num, ...) __M5_OP_INST(num, , __VA_ARGS__)
+#define M5_OP_INST_RET(num, ...) __M5_OP_INST(num, "=r" (_rax), __VA_ARGS__)
 
 // some of them return things i don't think this hack can really do ;-;
 #define M5_ARM_INST(address)                                M5_OP_INST(M5OP_ARM, "D" (address))
 #define M5_QUIESCE_INST()                                   M5_OP_INST(M5OP_QUIESCE)
 #define M5_QUIESCE_NS_INST(ns)                              M5_OP_INST(M5OP_QUIESCE_NS, "D" (ns))
 #define M5_QUIESCE_CYCLE_INST(cycles)                       M5_OP_INST(M5OP_QUIESCE_CYCLE, "D" (cycles))
-// #define M5_QUIESCE_TIME_INST()                              M5_OP_INST(M5OP_QUIESCE_TIME)
-// #define M5_RPNS_INST()                                      M5_OP_INST(M5OP_RPNS)
+#define M5_QUIESCE_TIME_INST()                              M5_OP_INST_RET(M5OP_QUIESCE_TIME)
+#define M5_RPNS_INST()                                      M5_OP_INST_RET(M5OP_RPNS)
 #define M5_WAKE_CPU_INST(cpuid)                             M5_OP_INST(M5OP_WAKE_CPU, "D" (cpuid))
 #define M5_EXIT_INST(ns_delay)                              M5_OP_INST(M5OP_EXIT, "D" (ns_delay))
 #define M5_FAIL_INST(ns_delay, code)                        M5_OP_INST(M5OP_FAIL, "D" (ns_delay), "S" (code))
 // #define M5_SUM_INST(a, b, c, d, e, f)                       M5_OP_INST(M5OP_SUM, )
-// #define M5_INIT_PARAM_INST(key_str1, key_str2)              M5_OP_INST(M5OP_INIT_PARAM, "D" (key_str1), "S" (key_str2))
+#define M5_INIT_PARAM_INST(key_str1, key_str2)              M5_OP_INST_RET(M5OP_INIT_PARAM, "D" (key_str1), "S" (key_str2))
 #define M5_CHECKPOINT_INST(ns_delay, ns_period)             M5_OP_INST(M5OP_CHECKPOINT, "D" (ns_delay), "S" (ns_period))
 #define M5_RESET_STATS_INST(ns_delay, ns_period)            M5_OP_INST(M5OP_RESET_STATS, "D" (ns_delay), "S" (ns_period))
 #define M5_DUMP_STATS_INST(ns_delay, ns_period)             M5_OP_INST(M5OP_DUMP_STATS, "D" (ns_delay), "S" (ns_period))
 #define M5_DUMP_RESET_STATS_INST(ns_delay, ns_period)       M5_OP_INST(M5OP_DUMP_RESET_STATS, "D" (ns_delay), "S" (ns_period))
-// #define M5_READ_FILE_INST(buffer, len, offset)              M5_OP_INST(M5OP_READ_FILE, "D" (buffer), "S" (len), "d" (offset))
-// #define M5_WRITE_FILE_INST(buffer, len, offset, filename)   M5_OP_INST(M5OP_WRITE_FILE, "D" (buffer), "S" (len), "d" (offset), "c" (filename))
+#define M5_READ_FILE_INST(buffer, len, offset)              M5_OP_INST_RET(M5OP_READ_FILE, "D" (buffer), "S" (len), "d" (offset))
+#define M5_WRITE_FILE_INST(buffer, len, offset, filename)   M5_OP_INST_RET(M5OP_WRITE_FILE, "D" (buffer), "S" (len), "d" (offset), "c" (filename))
 #define M5_DEBUG_BREAK_INST()                               M5_OP_INST(M5OP_DEBUG_BREAK)
 #define M5_SWITCH_CPU_INST()                                M5_OP_INST(M5OP_SWITCH_CPU)
 #define M5_DIST_TOGGLE_SYNC_INST()                          M5_OP_INST(M5OP_DIST_TOGGLE_SYNC)
@@ -126,19 +127,19 @@
 #define M5_QUIESCE()                                    M5_QUIESCE_INST()
 #define M5_QUIESCE_NS(ns)                               M5_QUIESCE_NS_INST(ns)
 #define M5_QUIESCE_CYCLE(cycles)                        M5_QUIESCE_CYCLE_INST(cycles)
-// #define M5_QUIESCE_TIME()                               M5_QUIESCE_TIME_INST()
-// #define M5_RPNS()                                       M5_RPNS_INST()
+#define M5_QUIESCE_TIME()                               M5_QUIESCE_TIME_INST()
+#define M5_RPNS()                                       M5_RPNS_INST()
 #define M5_WAKE_CPU(cpuid)                              M5_WAKE_CPU_INST(cpuid)
 #define M5_EXIT(ns_delay)                               M5_EXIT_INST(ns_delay)
 #define M5_FAIL(ns_delay, code)                         M5_FAIL_INST(ns_delay, code)
 // #define M5_SUM(a, b, c, d, e, f)                        M5_SUM_INST(a, b, c, d, e, f)
-// #define M5_INIT_PARAM(key_str1, key_str2)               M5_INIT_PARAM_INST(key_str1, key_str2)
+#define M5_INIT_PARAM(key_str1, key_str2)               M5_INIT_PARAM_INST(key_str1, key_str2)
 #define M5_CHECKPOINT(ns_delay, ns_period)              M5_CHECKPOINT_INST(ns_delay, ns_period)
 #define M5_RESET_STATS(ns_delay, ns_period)             M5_RESET_STATS_INST(ns_delay, ns_period)
 #define M5_DUMP_STATS(ns_delay, ns_period)              M5_DUMP_STATS_INST(ns_delay, ns_period)
 #define M5_DUMP_RESET_STATS(ns_delay, ns_period)        M5_DUMP_RESET_STATS_INST(ns_delay, ns_period)
-// #define M5_READ_FILE(buffer, len, offset)               M5_READ_FILE_INST(buffer, len, offset)
-// #define M5_WRITE_FILE(buffer, len, offset, filename)    M5_WRITE_FILE_INST(buffer, len, offset, filename)
+#define M5_READ_FILE(buffer, len, offset)               M5_READ_FILE_INST(buffer, len, offset)
+#define M5_WRITE_FILE(buffer, len, offset, filename)    M5_WRITE_FILE_INST(buffer, len, offset, filename)
 #define M5_DEBUG_BREAK()                                M5_DEBUG_BREAK_INST()
 #define M5_SWITCH_CPU()                                 M5_SWITCH_CPU_INST()
 #define M5_DIST_TOGGLE_SYNC()                           M5_DIST_TOGGLE_SYNC_INST()
@@ -149,34 +150,84 @@
 #define M5_WORK_END(workid, threadid)                   M5_WORK_END_INST(workid, threadid)
 #define M5_WORKLOAD()                                   M5_WORKLOAD_INST()
 
+#define M5_FUNC_RET(inline_asm) uint64_t _rax; inline_asm; return _rax;
 
+static inline void m5_arm(uint64_t address) { 
+    M5_ARM(address); 
+}
+static inline void m5_quiesce(void) {
+    M5_QUIESCE(); 
+}
+static inline void m5_quiesce_ns(uint64_t ns) { 
+    M5_QUIESCE_NS(ns); 
+}
+static inline void m5_quiesce_cycle(uint64_t cycles) { 
+    M5_QUIESCE_CYCLE(cycles); 
+}
+static inline uint64_t m5_quiesce_time(void) { 
+    M5_FUNC_RET(M5_QUIESCE_TIME()) 
+}
+static inline uint64_t m5_rpns() {
+    M5_FUNC_RET(M5_RPNS())
+}
+static inline void m5_wake_cpu(uint64_t cpuid) { 
+    M5_WAKE_CPU(cpuid); 
+}
 
-// static inline void m5_arm(uint64_t address) { M5_ARM(address); }
-// static inline void m5_quiesce(void) { M5_QUIESCE(); }
-// static inline void m5_quiesce_ns(uint64_t ns) { M5_QUIESCE_NS(ns); }
-// void m5_quiesce_cycle(uint64_t cycles);
-// uint64_t m5_quiesce_time(void);
-// uint64_t m5_rpns();
-// void m5_wake_cpu(uint64_t cpuid);
-
-// static inline void m5_exit(uint64_t ns_delay) { M5_EXIT(ns_delay); }
-// void m5_fail(uint64_t ns_delay, uint64_t code);
+static inline void m5_exit(uint64_t ns_delay) { 
+    M5_EXIT(ns_delay); 
+}
+static inline void m5_fail(uint64_t ns_delay, uint64_t code) { 
+    M5_FAIL(ns_delay, code); 
+}
 // unsigned m5_sum(unsigned a, unsigned b, unsigned c,
 //                 unsigned d, unsigned e, unsigned f);
-// uint64_t m5_init_param(uint64_t key_str1, uint64_t key_str2);
-// void m5_checkpoint(uint64_t ns_delay, uint64_t ns_period);
-// void m5_reset_stats(uint64_t ns_delay, uint64_t ns_period);
-// void m5_dump_stats(uint64_t ns_delay, uint64_t ns_period);
-// void m5_dump_reset_stats(uint64_t ns_delay, uint64_t ns_period);
-// uint64_t m5_read_file(void *buffer, uint64_t len, uint64_t offset);
-// uint64_t m5_write_file(void *buffer, uint64_t len, uint64_t offset,
-//                        const char *filename);
-// void m5_debug_break(void);
-// void m5_switch_cpu(void);
-// void m5_dist_toggle_sync(void);
-// void m5_add_symbol(uint64_t addr, const char *symbol);
-// void m5_load_symbol();
-// void m5_panic(void);
-// void m5_work_begin(uint64_t workid, uint64_t threadid);
-// void m5_work_end(uint64_t workid, uint64_t threadid);
-// void m5_workload();
+static inline uint64_t m5_init_param(uint64_t key_str1, uint64_t key_str2) { 
+    M5_FUNC_RET(M5_INIT_PARAM(key_str1, key_str2)) 
+}
+static inline void m5_checkpoint(uint64_t ns_delay, uint64_t ns_period) { 
+    M5_CHECKPOINT(ns_delay, ns_period); 
+}
+static inline void m5_reset_stats(uint64_t ns_delay, uint64_t ns_period) { 
+    M5_RESET_STATS(ns_delay, ns_period); 
+}
+static inline void m5_dump_stats(uint64_t ns_delay, uint64_t ns_period) { 
+    M5_DUMP_STATS(ns_delay, ns_period); 
+}
+static inline void m5_dump_reset_stats(uint64_t ns_delay, uint64_t ns_period) { 
+    M5_DUMP_RESET_STATS(ns_delay, ns_period); 
+}
+static inline uint64_t m5_read_file(void *buffer, uint64_t len, uint64_t offset) { 
+    M5_FUNC_RET(M5_READ_FILE(buffer, len, offset)) 
+}
+static inline uint64_t m5_write_file(void *buffer, uint64_t len, uint64_t offset,
+                       const char *filename) { 
+    M5_FUNC_RET(M5_WRITE_FILE(buffer, len, offset, filename)) 
+}
+static inline void m5_debug_break(void) { 
+    M5_DEBUG_BREAK(); 
+}
+static inline void m5_switch_cpu(void) { 
+    M5_SWITCH_CPU(); 
+}
+static inline void m5_dist_toggle_sync(void) { 
+    M5_DIST_TOGGLE_SYNC(); 
+}
+static inline void m5_add_symbol(uint64_t addr, const char *symbol) { 
+    M5_ADD_SYMBOL(addr, symbol); 
+}
+static inline void m5_load_symbol() { 
+    M5_LOAD_SYMBOL(); 
+}
+static inline void m5_panic(void) { 
+    M5_PANIC(); 
+}
+static inline void m5_work_begin(uint64_t workid, uint64_t threadid) { 
+    M5_WORK_BEGIN(workid, threadid); 
+}
+static inline void m5_work_end(uint64_t workid, uint64_t threadid) { 
+    M5_WORK_END(workid, threadid); 
+}
+static inline void m5_workload() { 
+    M5_WORKLOAD(); 
+}
