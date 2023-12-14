76e87d5a9214493b5397d531db5177c24972fbb9
zhizhuoxin <shixins@umich.edu>
2023-12-14 00:17:26 -0500

    Minor changes.

--- disk-image/experiments/experiments/install.sh
+++ disk-image/experiments/experiments/install.sh
@@ -12,18 +12,3 @@ cd lkmpg-example-modules
 make clean
 make all
 cd ..
-
-cd spectre-jz268
-make clean
-make all
-cd module-src
-make clean 
-make
-cd ..
-cd ..
-
-cd blindside
-make -B
-cd module-src
-make -B
-cd ../..
