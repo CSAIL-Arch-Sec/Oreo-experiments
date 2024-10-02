816ecec3d397578f54ad9e586d1129d47ad22534
shixins <shixins@mit.edu>
2024-10-01 20:36:45 -0400

    Cleanup: remove some other scripts.

--- clean_m5outs_uuid_dirs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# https://stackoverflow.com/questions/34057047/delete-all-directories-except-one
-shopt -s extglob
-cd m5outs
-rm -rf !(default-save)
--- copy_configs_to_gem5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp gem5-configs ../gem5/configs/example/gem5_library/gem5-configs -r
