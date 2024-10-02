1afc01d51cff5acc6de3c689ed864643d7a5d12a
shixins <shixins@mit.edu>
2024-10-01 21:15:47 -0400

    Cleanup: unused test benches and modules.

--- disk-image/experiments/experiments/install.sh
+++ disk-image/experiments/experiments/install.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-cd src
-python3 generate_large_header.py
-cd ..
-
 make clean
 make all
 
 cd modules
-make clean
-make all
-cd ..
-
-cd lkmpg-example-modules
 make clean
 make all
 cd ..
